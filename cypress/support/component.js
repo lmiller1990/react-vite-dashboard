ce9db7195b83c07975fb693301d57387403ebf5e
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-02-28 15:23:57 +1000

    wip

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -25,3 +25,52 @@ Cypress.Commands.add('mount', mount)
 
 // Example use:
 // cy.mount(<MyComponent />)
+import { faker } from '@faker-js/faker'
+
+// big module
+console.log(faker.address.city())
+
+import PropTypes from 'prop-types';
+import { useMemo } from 'react';
+// @mui
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
+//
+import palette from './palette';
+import shadows from './shadows';
+import typography from './typography';
+import GlobalStyles from './globalStyles';
+import customShadows from './customShadows';
+import componentsOverride from './overrides';
+
+// ----------------------------------------------------------------------
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default function ThemeProvider({ children }) {
+  const themeOptions = useMemo(
+    () => ({
+      palette,
+      shape: { borderRadius: 6 },
+      typography,
+      shadows: shadows(),
+      customShadows: customShadows(),
+    }),
+    []
+  );
+
+  const theme = createTheme(themeOptions);
+  theme.components = componentsOverride(theme);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <MUIThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        {children}
+      </MUIThemeProvider>
+    </StyledEngineProvider>
+  );
+}
